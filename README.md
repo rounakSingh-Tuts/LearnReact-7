00f633226c560b3e413e9be74eced441b268e529
rounakSingh-Tuts <rounak.techinfo@gmail.com>
2019-11-01 17:09:45 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-r4jlzi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-r4jlzi)
---
+++ components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Mohan</h1>
+      <p>Paragraph about me..</p>
+      <ul>
+        <li>Agra</li>
+        <li>Shimla</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
